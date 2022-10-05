c8493ea66379fd25a844e58d07b1288a43462e11
Ronit Cohen <ron13795@gmail.com>
2022-10-05 19:51:58 +0300

    drf

--- employees/src/Components/Menu/Menu.jsx
+++ employees/src/Components/Menu/Menu.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import "./Menu.css";
+import context from "../../context";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Menu() {
+  const nav = useNavigate();
+  const { setName, name, setShowMenu, tasks, tasksToShow, setTasks } =
+    useContext(context);
+
+  const allTasks = () => {
+    setTasks(tasks);
+  };
+
+  const myTasks = () => {
+    const myTask = tasks.filter((task) => {
+      return task.employee === name;
+    });
+    setTasks(myTask);
+  };
+
+  const exit = () => {
+    setShowMenu(false);
+    setName("");
+    nav("/");
+  };
+
   return (
     <div
       style={{
@@ -10,11 +34,11 @@ export default function Menu() {
         width: "20%",
       }}
     >
-      <button>כל המשימות</button>
-      <button>המשימות שלי </button>
+      <button onClick={allTasks}>כל המשימות</button>
+      <button onClick={myTasks}>המשימות שלי </button>
       <button> משימה חדשה</button>
       <button>היסטוריה </button>
-      <button>יציאה </button>
+      <button onClick={exit}>יציאה </button>
     </div>
   );
 }
